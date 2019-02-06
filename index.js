78dd8ed29907b1d451b2a06ee1ae74d93e6d0307
Donna Amos <donnacamos.developer@gmail.com>
2019-02-06 23:56:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,30 +7,30 @@ function subtract(a, b){
 }
 
 function multiply(a, b){
-  return `a * b`
+  return a * b;
 }
 
 function divide(a, b){
-  return `a / b`
+  return a / b;
 }
 
 
 function inc(a){
-  return `a + 1`
+  return a + 1;
 }
 
 function dec(a){
-  return `a - 1`
+  return a - 1;
 }
 
 function makeInt(a){
-  return `makeInt(a.toString())`
+  return makeInt(a.toString());
 }
 
 
 
 function preserveDecimal(a){
-  return `preserveDecimal('2.222')`
+  return preserveDecimal('2.222');
 }
 
 
